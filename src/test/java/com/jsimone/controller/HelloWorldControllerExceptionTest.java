325d8c9f584e6cb657abb5b93c2f23e3fd128f69
Joe Simone <jsimone@000926-Jsimone.local>
2019-03-15 14:54:24 -0400

    add test for endpoint exception

--- src/test/java/com/jsimone/controller/HelloWorldControllerExceptionTest.java
+++ src/test/java/com/jsimone/controller/HelloWorldControllerExceptionTest.java
@@ -1,6 +1,8 @@
 package com.jsimone.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jsimone.app.Application;
+import com.jsimone.error.ErrorResponseBody;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloWorldControllerExceptionTest {
 
-    private static String CONTENT_TYPE = "text/plain;charset=UTF-8";
+    private static String CONTENT_TYPE = "application/json;charset=UTF-8";
 
     @LocalServerPort
     private int port;
@@ -25,23 +28,22 @@ public class HelloWorldControllerExceptionTest {
 
     @Test
     public void test1() throws Exception {
-        String url = "http://localhost:" + port + "/hello/tom";
+        String url = "http://localhost:" + port + "/hello/";
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         assertEquals(CONTENT_TYPE, response.getHeaders().getContentType().toString());
-        assertEquals("hello tom", response.getBody());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = response.getBody();
+        ErrorResponseBody errorResponse = objectMapper.readValue(json, ErrorResponseBody.class);
+        assertEquals(HttpStatus.NOT_FOUND.value(), errorResponse.getCode());
+        assertEquals("/hello/", errorResponse.getPath());
+        assertEquals("The URL you have reached is not in service at this time", errorResponse.getMessage());
+        assertTrue(errorResponse.getErrors().isEmpty());
     }
 
-    @Test
-    public void test2() throws Exception {
-        String url = "http://localhost:" + port + "/hello2?name=fred";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(CONTENT_TYPE, response.getHeaders().getContentType().toString());
-        assertEquals("hello fred", response.getBody());
-    }
 }
 
 

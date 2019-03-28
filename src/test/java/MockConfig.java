8a73bc51fc24cad9f275280090b6a2255df87343
jesimone57 <jesimone57@yahoo.com>
2019-03-28 12:50:46 -0400

    add karate mock servlet to run all karate tests without instantiating a server

---
+++ src/test/java/MockConfig.java
@@ -0,0 +1,55 @@
+import com.jsimone.controller.HelloWorldController;
+import com.jsimone.controller.PalindromicNumberController;
+import com.jsimone.controller.PrimeNumberController;
+import com.jsimone.service.CommonNumberService;
+import com.jsimone.service.PalindromicNumberService;
+import com.jsimone.service.PrimeNumberService;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@EnableAutoConfiguration
+@PropertySource("classpath:application-test.properties")
+public class MockConfig {
+
+    @Bean
+    public HelloWorldController helloWorldControlller() {
+        return new HelloWorldController();
+    }
+
+    @Bean
+    public PrimeNumberService primeNumberService() {
+        return new PrimeNumberService();
+    }
+
+    @Bean
+    public CommonNumberService commonNumberService() {
+        return new CommonNumberService();
+    }
+
+    @Bean
+    public PrimeNumberController primeNumberController() {
+        return new PrimeNumberController();
+    }
+
+    @Bean
+    public PalindromicNumberService palindromicNumberService() {
+        return new PalindromicNumberService();
+    }
+
+    @Bean
+    public PalindromicNumberController palindromicNumberController() {
+        return new PalindromicNumberController();
+    }
+
+    /*
+    @Bean
+    public AmicableNumberController amicableNumberController() {
+        return new AmicableNumberController();
+    }
+    */
+
+}
